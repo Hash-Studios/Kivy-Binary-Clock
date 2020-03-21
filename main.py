2d53bcbaa533d152542f22031d26dd4cbbddbaa7
Abhay Maurya <maurya.abhay30@gmail.com>
2020-03-22 01:20:14 +0530

    Added some basic kv

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 import kivy
-from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.config import Config
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 
+from kivymd.app import MDApp
+from kivymd.theming import ThemableBehavior
+
 kivy.require("2.0.0")
+
+KV = '''
+#:import NavigationDrawerToolbar kivymd.navigationdrawer.NavigationDrawerToolbar
+NavigationLayout:
+    MDNavigationDrawer:
+        drawer_logo: "C:/User/Asus/Desktop/Kivy-Chat-App/icon.png"
+        spacing: 8
+        NavigationDrawerToolbar:
+            elevation: 8
+            title: "Binary Clock"
+'''
 
 
 class ClockApp(MDApp):
     def build(self):
         app = MDApp.get_running_app()
-        self.grid_layout = GridLayout()
-        return self.grid_layout
+        app.theme_cls.primary_palette = "DeepOrange"
+        app.theme_cls.accent_palette = "DeepOrange"
+        app.theme_cls.theme_style = "Dark"
+        Window.size = (640, 360)
+        Window.borderless = False
+        self.title = "Binary Clock"
+        Config.set('kivy', 'window_title', 'Binary Clock')
+        return Builder.load_string(KV)
 
 
 if __name__ == "__main__":
