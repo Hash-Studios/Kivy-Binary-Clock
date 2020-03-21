376653c99d1b23fa9cf225b67c31800dd72ba6e3
Abhay Maurya <maurya.abhay30@gmail.com>
2020-03-21 20:24:19 +0530

    Minor changes

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import kivy
-from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.gridlayout import GridLayout
 
 kivy.require("2.0.0")
 
 
-class ClockApp(App):
+class ClockApp(MDApp):
     def build(self):
-        app = App.get_running_app()
+        app = MDApp.get_running_app()
         self.grid_layout = GridLayout()
         return self.grid_layout
+
 
 if __name__ == "__main__":
     clock_app = ClockApp()
