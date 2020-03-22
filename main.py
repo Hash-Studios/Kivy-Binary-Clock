f6d68bd4a8237a10e53f87a942dd9c7ac5c98c10
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-03-22 22:34:17 +0530

    Added Nav Drawer and added two screens

--- main.py
+++ main.py
@@ -4,31 +4,77 @@ from kivy.core.window import Window
 from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty
 
 from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
 
-kivy.require("2.0.0")
+#kivy.require("2.0.0")
 
 KV = '''
-#:import NavigationDrawerToolbar kivymd.navigationdrawer.NavigationDrawerToolbar
-NavigationLayout:
-    MDNavigationDrawer:
-        drawer_logo: "C:/User/Asus/Desktop/Kivy-Chat-App/icon.png"
-        spacing: 8
-        NavigationDrawerToolbar:
-            elevation: 8
-            title: "Binary Clock"
-'''
+<ContentNavigationDrawer>:
+	ScrollView:
+		MDList:
+		    OneLineListItem:
+		        text: "Binary Clock"
+		        on_press:
+		            root.nav_drawer.set_state("close")
+		            root.screen_manager.current = "Clock"
+		    OneLineListItem:
+		        text: "Settings"
+		        on_press:
+		            root.nav_drawer.set_state("close")
+		            root.screen_manager.current = "Settings"
 
+Screen:
+	MDToolbar:
+		id: toolbar
+		pos_hint: {"top": 1}
+		elevation: 8
+		title: "Binary Clock"
+		left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+	NavigationLayout:
+		x: toolbar.height
+		ScreenManager:
+			id: screen_manager
+			Screen:
+				name: "Clock"
+				GridLayout:
+					cols: 6
+					MDLabel:
+						text: "H"
+					MDLabel:
+						text: "H"
+					MDLabel:
+						text: "M"
+					MDLabel:
+						text: "M"
+					MDLabel:
+						text: "S"
+					MDLabel:
+						text: "S"
+			Screen:
+				name: "Settings"
+				Label:
+					text: "Settings"
+		MDNavigationDrawer:
+			id: nav_drawer
+			
+			ContentNavigationDrawer:
+				screen_manager: screen_manager
+				nav_drawer: nav_drawer
+			
+'''
+class ContentNavigationDrawer(BoxLayout):
+	screen_manager = ObjectProperty()
+	nav_drawer = ObjectProperty()
 
 class ClockApp(MDApp):
     def build(self):
         app = MDApp.get_running_app()
-        app.theme_cls.primary_palette = "DeepOrange"
-        app.theme_cls.accent_palette = "DeepOrange"
+        app.theme_cls.primary_palette = "Amber"
+        app.theme_cls.accent_palette = "Amber"
         app.theme_cls.theme_style = "Dark"
-        Window.size = (640, 360)
         Window.borderless = False
         self.title = "Binary Clock"
         Config.set('kivy', 'window_title', 'Binary Clock')
