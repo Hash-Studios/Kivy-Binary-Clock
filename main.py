53ec90e9ecf4acfe47ab89a86c692e44a935d7cd
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-03-23 00:06:05 +0530

    Added dynamic updating clock

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import kivy
+import time
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
+from kivy.clock import Clock
 
 from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
@@ -12,6 +15,9 @@ from kivymd.theming import ThemableBehavior
 #kivy.require("2.0.0")
 
 KV = '''
+#:import time time
+#:import Clock kivy.clock.Clock
+#:import Label kivy.uix.label.Label
 <ContentNavigationDrawer>:
 	ScrollView:
 		MDList:
@@ -25,38 +31,29 @@ KV = '''
 		        on_press:
 		            root.nav_drawer.set_state("close")
 		            root.screen_manager.current = "Settings"
-
+<Clock_Widget>:
+	padding: 10
+	time_rn: "Hello"
+	MDLabel:
+		text: root.time_rn
 Screen:
 	MDToolbar:
 		id: toolbar
 		pos_hint: {"top": 1}
 		elevation: 8
 		title: "Binary Clock"
-		left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+		left_action_items: [["clock", lambda x: nav_drawer.set_state("open")]]
 	NavigationLayout:
 		x: toolbar.height
 		ScreenManager:
 			id: screen_manager
 			Screen:
-				name: "Clock"
-				GridLayout:
-					cols: 6
-					MDLabel:
-						text: "H"
-					MDLabel:
-						text: "H"
-					MDLabel:
-						text: "M"
-					MDLabel:
-						text: "M"
-					MDLabel:
-						text: "S"
-					MDLabel:
-						text: "S"
-			Screen:
 				name: "Settings"
-				Label:
+				MDLabel:
 					text: "Settings"
+			Screen:
+				name: "Clock"
+				Clock_Widget:
 		MDNavigationDrawer:
 			id: nav_drawer
 			
@@ -68,7 +65,13 @@ Screen:
 class ContentNavigationDrawer(BoxLayout):
 	screen_manager = ObjectProperty()
 	nav_drawer = ObjectProperty()
-
+class Clock_Widget(BoxLayout):
+	time_rn = StringProperty(time.asctime())
+	def __init__(self, **kwargs):
+		super(Clock_Widget, self).__init__(**kwargs)
+		Clock.schedule_interval(self.update, 1.0 / 60.0)
+	def update(self, dt):
+		self.time_rn = time.asctime()
 class ClockApp(MDApp):
     def build(self):
         app = MDApp.get_running_app()
